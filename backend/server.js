092b06ff91f7a31f4a51f3196a9fa9f87e28edad
Sean <seanzhu@Seans-Mac-mini.local>
2022-09-30 16:13:49 -0600

    Complete backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express') 
+const colors = require('colors')
 const dotenv = require('dotenv').config() 
-
+const {errorHandler} = require('./middleware/errorMiddle')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000
 
+// Connect to database 
+connectDB()
+
 const app = express() 
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/users', (req, res) => {
     res.status(200).json({message: 'Welcome to the Support Desk API'})
@@ -11,5 +19,7 @@ app.get('/api/users', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) 
