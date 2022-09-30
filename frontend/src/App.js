8048f1d44cb1648d5fca70085cac66e9bd18cbf6
Sean <seanzhu@Seans-Mac-mini.local>
2022-09-30 17:36:59 -0600

    Added basic routes in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';  
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div> 
-       My App
-    </div>
+    <> 
+       <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} /> 
+            <Route path='/login' element={<Login />} /> 
+            <Route path='/register' element={<Register />} /> 
+          </Routes>
+        </div>
+       </Router>
+    </>
   );
 }
 
