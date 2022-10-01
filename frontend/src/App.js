cb669f8fe1bf6de5a39c59561643b199a9f9f5b9
Sean <seanzhu@Seans-Mac-mini.local>
2022-10-01 15:31:48 -0600

    Home, register and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';  
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
+
 
 function App() {
   return (
@@ -19,6 +22,7 @@ function App() {
           </Routes>
         </div>
        </Router>
+       <ToastContainer />
     </>
   );
 }
