a253154328dae52aefa242ce829a2c129bb21441
Sean <seanzhu@Seans-Mac-mini.local>
2022-10-05 15:50:34 -0600

    Backend ticket function and front end create ticket

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-import {toast} from 'react-toastify' 
-
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login, reset } from "../features/auth/authSlice"; 
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 function Login() {
   const [formData, setFormData] = useState({
     email: "",
-    password: "", 
+    password: "",
   });
 
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
-  };
+  }
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    // Redirect when logged in 
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+ 
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onSubmit = (e) => {
-    e.preventDefault()
- 
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
+  }
+
+  if(isLoading) {
+    return <Spinner />
   }
 
   return (
@@ -33,8 +67,7 @@ function Login() {
       </section>
 
       <section className="form">
-        <form  onSubmit={onSubmit}>
-           
+        <form onSubmit={onSubmit}>
           <div className="form-group">
             <input
               type="email"
@@ -59,7 +92,7 @@ function Login() {
               required
             />
           </div>
-          
+
           <div className="form-group">
             <button className="btn btn-block">Submit</button>
           </div>
